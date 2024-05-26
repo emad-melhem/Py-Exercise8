09f7949400eb939abe7d47cfa33426c67cf03396
Imad Mohamad <yehya.mh@live.com>
2024-05-26 14:39:15 +0200

    add comment to exercise 3

--- Exercise3.py
+++ Exercise3.py
@@ -1,9 +1,6 @@
    # Exercise 3.1
-def indexOf_letter(letter):
-    letter = str(letter)
-    letters = []  
-    for i in range(97, 123):  
-        letters.append(chr(i))
+def indexOf_letter(letter): #return the index of a letter and return -1 if not in it.
+    letters = list(map(chr ,range(97, 123))) # creat list of lower letters.
     if letter.lower() in letters:
         return letters.index(letter.lower()) + 1
     else:
@@ -15,12 +12,10 @@ print(indexOf_letter('z'))
 
    # Exercise 3.2
 def is_pangram(text):
-    letters = []  
-    for i in range(97, 123):  
-        letters.append(chr(i))
+    letters = list(map(chr ,range(97, 123))) # creat list of lower letters.
     for letter in text:
         if letter.lower() in letters:
-            letters.remove(letter)
-    return len(letters) == 0
+            letters.remove(letter) # remove the same letter from letters to get the letters that are not present in the text.
+    return len(letters) == 0 # True if all letters are present in the text.otherwise False
 
 print(is_pangram('abcdefghijklmnopqrstuvwxyz'))
