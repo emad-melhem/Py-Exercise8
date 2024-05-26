333d3f2459fd6895acf5d4afe28f325486f2aa27
Imad Mohamad <yehya.mh@live.com>
2024-05-26 14:46:10 +0200

    Modified Exercise 2

--- Exercise2.py
+++ Exercise2.py
@@ -1,14 +1,8 @@
 def Get_List(num1, num2): # return list from num1 to num2 excluding num2
-    num1 = int(num1)
-    num2 = int(num2)
-    lst =[]
     change_rate = 1
     if num1 > num2:
         change_rate = -1
-    current_num = num1
-    while current_num != num2:
-        lst.append(current_num)
-        current_num += change_rate
+    lst = list(range(num1, num2, change_rate))
     return lst
 
-print(Get_List(-5, 2))
+print(Get_List(9, 2))
